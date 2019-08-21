ce1085d1fb556de4e8ad66884e8d69d1ed69dbdd
Caetano <caetanov120@gmail.com>
2019-08-21 12:42:01 -0300

    Created Files
    * Added package behaviors and communications
    * Added config file

---
+++ config/jids.py
@@ -0,0 +1,5 @@
+AGENT1_EMAIL = 'caetano@conversations.im'
+AGENT1_PASSWORD = 'caetano123'
+
+AGENT2_EMAIL = 'joyce@conversations.im'
+AGENT2_PASSWORD = 'joyce123'
---
+++ src/dummy_agent.py
@@ -0,0 +1,11 @@
+from spade import agent
+import config.jids as jid
+
+
+class DummyAgent(agent.Agent):
+    async def setup(self):
+        print("Hello World! I'm agent {}".format(str(self.jid)))
+
+
+dummy = DummyAgent(jid.AGENT1_EMAIL, jid.AGENT1_PASSWORD)
+dummy.start()
